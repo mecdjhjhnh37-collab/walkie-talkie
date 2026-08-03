3b29c7aecd915f9e10ff4ae82c7324f6fe7261ba
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-03 22:39:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,12 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
-const fs = require('fs');
 const path = require('path');
 
-app.use(express.static(path.join(process.cwd(), 'عام')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-    const indexPath = path.join(process.cwd(), 'عام', 'index.html');
-    if (fs.existsSync(indexPath)) {
-        res.sendFile(indexPath);
-    } else {
-        res.send('<h1>جاري تجهيز الواجهة، يرجى الانتظار ثواني...</h1>');
-    }
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
