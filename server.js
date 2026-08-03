0cb7e048163f111f17957c4ff1b53364330da37d
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-03 22:24:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 const path = require('path');
 
+// قراءة الملفات الثابتة من مجلد عام
 app.use(express.static(path.join(__dirname, 'عام')));
 
+// إرسال صفحة الواجهة الرئيسية
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'عام', 'index.html'));
 });
@@ -19,6 +21,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT);
-
-
+server.listen(PORT, () => {
+    console.log('Server is running');
+});
